70029322c2deb4128a0ceacda2f90276b9c48d90
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-09-03 21:29:03 +0300

    setup project

---
+++ Lesson3.02/ViewControllers/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CollectionViewController.swift
+//  Lesson3.02
+//
+//  Created by Goodwasp on 03.09.2023.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class MainViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
+}
